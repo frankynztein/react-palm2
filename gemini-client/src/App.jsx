dd7dda3402402fb6a05e2ebd846ed5b658919c8c
frankynztein <arellanfranciree@gmail.com>
2024-02-11 18:01:08 -0500

    This is working

---
+++ gemini-client/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect, useRef } from 'react'
+import DOMPurify from 'dompurify'
+import { marked } from 'marked'
+
+marked.use({
+  gfm: true,
+})
+
+function App() {
+
+  const [serverData, setServerData] = useState([{}]);
+  const [userPrompt, setUserPrompt] = useState('');
+  const inputRef = useRef(null);
+
+  function handleKeyDown(e) {
+    if(e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+    }
+  }
+
+  function handleSubmit () {
+    setServerData('');
+    if(userPrompt !== '') {
+      fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'prompt':userPrompt})
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        const html = DOMPurify.sanitize(marked(data));
+        setServerData({...data, html})
+        inputRef.current.focus();
+        setUserPrompt('');
+        console.log(data)
+      })
+    }
+  }
+
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setServerData(data)
+  //       console.log(data)
+  //     })
+  // }, [])
+
+  return (
+    <main style={{height:'100vh', display: 'flex', flexDirection: 'column'}}>
+      <h1 style={{padding:'10px', marginBottom: '0'}}>My prompter</h1>
+      <div style={{margin: '0', flexGrow: '1', overflow: 'scroll'}}>
+        <div style={{width: '100%', height: '100%'}}>
+          {/* <article style={{margin: '0'}}>{serverData}</article> */}
+
+          {
+            (serverData === '') ? ('Loading') : <article style={{margin: '0'}} dangerouslySetInnerHTML={{__html: serverData.html}}/>
+          }
+        </div>
+      </div>
+      <div style={{display: 'flex', alignItems: 'end', backgroundColor: '#222', padding:'10px'}}>
+        <textarea 
+          style={{margin: '0', flexGrow: '1', overflowY: 'hidden'}} 
+          placeholder='Type in prompt'
+          onChange={(e) => setUserPrompt(e.target.value)}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+          value={userPrompt}
+          ></textarea>
+        <button style={{margin: '0', flex: '1', marginLeft: '10px'}} onClick={handleSubmit}>Go</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
